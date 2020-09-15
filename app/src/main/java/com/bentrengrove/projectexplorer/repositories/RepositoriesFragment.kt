2f54c9d6d792e060184cd76fbe7ea1f290ff7d1d
Ben Trengrove <ben@pocketcasts.com>
2020-09-15 20:40:49 +1000

    Load coil images

--- app/src/main/java/com/bentrengrove/projectexplorer/repositories/RepositoriesFragment.kt
+++ app/src/main/java/com/bentrengrove/projectexplorer/repositories/RepositoriesFragment.kt
@@ -4,45 +4,33 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.ProgressBar
-import android.widget.TextView
 import androidx.compose.foundation.Text
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.RowScope.gravity
 import androidx.compose.foundation.lazy.LazyColumnFor
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.ui.platform.ComposeView
-import androidx.compose.ui.viewinterop.viewModel
-import androidx.core.app.SharedElementCallback
-import androidx.core.view.ViewCompat
-import androidx.core.view.isVisible
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.fragment.FragmentNavigatorExtras
-import androidx.navigation.fragment.findNavController
-import androidx.recyclerview.widget.RecyclerView
-import com.bentrengrove.RepositoriesQuery
-import com.bentrengrove.projectexplorer.R
-import com.bentrengrove.projectexplorer.theme.ProjectTheme
-import com.bentrengrove.projectexplorer.util.SimpleItemAdapter
-import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import javax.inject.Inject
-import kotlin.coroutines.CoroutineContext
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.viewModel
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import androidx.ui.tooling.preview.Preview
+import com.bentrengrove.projectexplorer.theme.ProjectTheme
+import dagger.hilt.android.AndroidEntryPoint
+import dev.chrisbanes.accompanist.coil.CoilImage
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlin.coroutines.CoroutineContext
 
 @AndroidEntryPoint
 class RepositoriesFragment : Fragment(), CoroutineScope {
@@ -53,7 +41,7 @@ class RepositoriesFragment : Fragment(), CoroutineScope {
         return ComposeView(requireContext()).apply {
             setContent {
                 RepositoriesScreen(onItemClick = {
-                    itemSelected(it.toSimpleItem())
+                    itemSelected(it)
                 })
             }
         }
@@ -66,7 +54,7 @@ class RepositoriesFragment : Fragment(), CoroutineScope {
 }
 
 @Composable
-fun RepositoriesScreen(onItemClick: (RepositoriesQuery.Node) -> Unit) {
+fun RepositoriesScreen(onItemClick: (RepositorySimpleItem) -> Unit) {
     ProjectTheme {
         val viewModel: RepositoriesViewModel = viewModel()
         val state by viewModel.repositories.observeAsState()
@@ -76,7 +64,7 @@ fun RepositoriesScreen(onItemClick: (RepositoriesQuery.Node) -> Unit) {
                 LoadingProgress()
             }
             is RepositoriesViewState.Loaded -> {
-                RepositoriesList(repositories = (state as RepositoriesViewState.Loaded).repositories, onItemClick)
+                RepositoriesList(repositories = (state as RepositoriesViewState.Loaded).repositories.map { it.toSimpleItem() }, onItemClick)
             }
             is RepositoriesViewState.LoadedEmpty -> {
                 EmptyListView()
@@ -92,28 +80,39 @@ fun RepositoriesScreen(onItemClick: (RepositoriesQuery.Node) -> Unit) {
 
 @Composable
 fun LoadingProgress() {
-    Text("Loading...")
+    CircularProgressIndicator(modifier = Modifier.size(48.dp).gravity(Alignment.CenterVertically))
 }
 
 @Composable
-fun RepositoriesList(repositories: List<RepositoriesQuery.Node>, onItemClick: (RepositoriesQuery.Node) -> Unit) {
+fun RepositoriesList(repositories: List<RepositorySimpleItem>, onItemClick: (RepositorySimpleItem) -> Unit) {
     LazyColumnFor(items = repositories) { item ->
         RepositoryItem(item = item, onItemClick)
     }
 }
 
 @Composable
-fun RepositoryItem(item: RepositoriesQuery.Node, onClick: (RepositoriesQuery.Node) -> Unit) {
+fun RepositoryItem(item: RepositorySimpleItem, onClick: (RepositorySimpleItem) -> Unit) {
     Row(modifier = Modifier.fillMaxWidth().clickable(onClick = { onClick(item) })) {
-        Surface(shape = CircleShape,
-            modifier = Modifier.padding(8.dp).size(56.dp),
-            color = MaterialTheme.colors.primary) {}
+        if (item.imageUri != null) {
+            CoilImage(data = item.imageUri.toString(), modifier = Modifier.padding(8.dp).size(56.dp).clip(CircleShape))
+        } else {
+            Surface(shape = CircleShape,
+                modifier = Modifier.padding(8.dp).size(56.dp),
+                color = MaterialTheme.colors.primary) {}
+        }
 
         Column(modifier = Modifier.gravity(Alignment.CenterVertically)) {
-            Text(text = item.name)
-            Text(text = item.owner.login)
+            Text(text = item.title, style = MaterialTheme.typography.body1)
+            Text(text = item.owner, style = MaterialTheme.typography.caption)
         }
     }
+}
+
+@Preview
+@Composable
+fun RepositoryItemPreview() {
+    val item = RepositorySimpleItem("1", "Project", "Jo Bloggs", null, "Jo Bloggs")
+    RepositoryItem(item = item, onClick = {})
 }
 
 @Composable
