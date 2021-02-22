57c2dbb941ec97ef558b8ac4be0803f26b1a0092
Ben Trengrove <ben@pocketcasts.com>
2021-02-23 08:02:25 +1000

    WIP compose viewpager

--- app/src/main/java/com/bentrengrove/projectexplorer/project/ProjectFragment.kt
+++ app/src/main/java/com/bentrengrove/projectexplorer/project/ProjectFragment.kt
@@ -6,90 +6,39 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.ProgressBar
-import android.widget.TextView
-import androidx.core.view.isVisible
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.viewModel
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
-import androidx.lifecycle.Observer
-import androidx.navigation.fragment.navArgs
-import androidx.viewpager2.widget.ViewPager2
 import com.bentrengrove.ProjectQuery
-import com.bentrengrove.projectexplorer.R
-import com.google.android.material.tabs.TabLayout
-import com.google.android.material.tabs.TabLayoutMediator
+import com.bentrengrove.projectexplorer.projects.EmptyListView
+import com.bentrengrove.projectexplorer.projects.ErrorView
+import com.bentrengrove.projectexplorer.projects.ProjectsViewState
+import com.bentrengrove.projectexplorer.repositories.LoadingProgress
+import com.bentrengrove.projectexplorer.theme.ProjectTheme
+import com.bentrengrove.projectexplorer.util.Pager
+import com.bentrengrove.projectexplorer.util.PagerState
 import dagger.hilt.android.AndroidEntryPoint
+import kotlin.random.Random
 
 @AndroidEntryPoint
 class ProjectFragment: Fragment() {
-    private val viewModel by viewModels<ProjectViewModel>()
-    private val args: ProjectFragmentArgs by navArgs()
-    private var tabMediator: TabLayoutMediator? = null
-    private lateinit var viewPagerAdapter: ProjectPagerAdapter
-
-    private lateinit var viewPager: ViewPager2
-    private lateinit var loadingProgress: ProgressBar
-    private lateinit var errorLayout: ViewGroup
-    private lateinit var imgError: ImageView
-    private lateinit var lblError: TextView
-    private lateinit var tabLayout: TabLayout
-
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.project_fragment, container, false)
-
-        viewPager = view.findViewById(R.id.viewPager)
-        loadingProgress = view.findViewById(R.id.loadingProgress)
-        errorLayout = view.findViewById(R.id.errorLayout)
-        imgError = view.findViewById(R.id.imgError)
-        lblError = view.findViewById(R.id.lblError)
-        tabLayout = view.findViewById(R.id.tabLayout)
-
-        return view
-    }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-
-        viewPagerAdapter = ProjectPagerAdapter(this::cardClicked)
-        viewPager.adapter = viewPagerAdapter
-
-        viewModel.project.observe(viewLifecycleOwner, Observer {
-            when (it) {
-                is ProjectViewState.Loading -> {
-                    loadingProgress.isVisible = true
-                    errorLayout.isVisible = false
-                }
-                is ProjectViewState.Loaded -> {
-                    setupForProject(it.project)
-
-                    loadingProgress.isVisible = false
-                    errorLayout.isVisible = false
-                }
-                is ProjectViewState.LoadedEmpty -> {
-                    imgError.setImageResource(R.drawable.ic_list_black_24dp)
-                    lblError.text = getString(R.string.project_empty)
-                    loadingProgress.isVisible = false
-                    errorLayout.isVisible = true
-                }
-                is ProjectViewState.Error -> {
-                    imgError.setImageResource(R.drawable.ic_error_outline_black_24dp)
-                    lblError.text = it.error.message
-
-                    loadingProgress.isVisible = false
-                    errorLayout.isVisible = true
-                }
+        return ComposeView(requireContext()).apply {
+            setContent {
+                ProjectScreen(onItemClick = this@ProjectFragment::cardClicked)
             }
-        })
-    }
-
-    private fun setupForProject(project: ProjectQuery.Project) {
-        tabMediator?.detach()
-        tabMediator = TabLayoutMediator(tabLayout, viewPager) { tab, position ->
-            tab.text = project.columns.nodes?.get(position)?.name
-        }.apply { attach() }
-
-        viewPagerAdapter.submitList(project.columns.nodes)
+        }
     }
 
     private fun cardClicked(card: ProjectQuery.Node1) {
@@ -102,4 +51,47 @@ class ProjectFragment: Fragment() {
             startActivity(intent)
         }
     }
+}
+
+@Composable
+fun ProjectScreen(onItemClick: (ProjectQuery.Node1) -> Unit) {
+    ProjectTheme {
+        val viewModel = viewModel<ProjectViewModel>()
+        val state by viewModel.project.observeAsState()
+
+        when (state) {
+            ProjectViewState.Loading -> {
+                LoadingProgress(Modifier.wrapContentSize())
+            }
+            is ProjectViewState.Loaded -> {
+                val project = (state as ProjectViewState.Loaded).project
+                ProjectPager(project = project, onItemClick = onItemClick)
+            }
+            ProjectViewState.LoadedEmpty -> EmptyListView()
+            is ProjectViewState.Error -> ErrorView(error = (state as ProjectsViewState.Error).message ?: "Unknown error")
+            null -> { }
+        }
+    }
+}
+
+@Composable
+fun ProjectPager(project: ProjectQuery.Project, onItemClick: (ProjectQuery.Node1) -> Unit) {
+    val columns = project.columns.nodes?.filterNotNull() ?: listOf()
+    val pagerState = remember { PagerState(maxPage = columns.size-1) }
+
+    Pager(state = pagerState, modifier = Modifier.fillMaxWidth().fillMaxHeight()) {
+        val item = columns[page]
+        ProjectColumn(column = item)
+    }
+}
+
+@Composable
+fun ProjectColumn(column: ProjectQuery.Node) {
+    val color = remember {
+        val random = Random.Default
+        Color(random.nextInt(255), random.nextInt(255), random.nextInt(255))
+    }
+    Box(modifier = Modifier.background(color).padding(4.dp)
+        .fillMaxHeight().fillMaxWidth())
+
 }
